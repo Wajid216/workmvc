ed9bde8b27f2570f1ec1f4e0e423ac7a1419be2c
Wajid216 <ali.wajid216@gmail.com>
2018-01-16 23:29:32 +0500

    work

---
+++ WebApplication8/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        w3css_templates_clothing_storeEntities db = new w3css_templates_clothing_storeEntities();
+        public ActionResult Index()
+        {
+            var product = db.Products.ToList();
+            ViewBag.products = product;
+            return View();
+        }
+        public ActionResult Category()
+        {
+            var category = db.Categories.ToList();
+            ViewBag.categoirs = category;
+            return View();
+        }
+        public ActionResult GetPro(int CategoryId)
+        {
+            var product = db.Products.Where(p => p.CategoryId == CategoryId).ToList();
+            ViewBag.products = product;
+            return View("Index");
+        }
+        public ActionResult Sub(Subcribe obj)
+        {
+            db.Subcribes.Add(obj);
+            db.SaveChanges();
+            return View("Index");
+        }
+        public ActionResult Tbl(Subcribe obj)
+        {
+            db.Subcribes.Add(obj);
+            db.SaveChanges();
+            return View("Index");
+        }
+	}
+}
